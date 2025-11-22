04bd3fd690544114e9d8e5f36a4cedbae1eae678
Maria Jose Roldan Cuellar <roldancuellarmariajose@gmail.com>
2025-11-22 10:47:36 -0500

    base de datos completa

--- database.sql
+++ database.sql
@@ -43,19 +43,34 @@ CREATE TABLE t_producto(
 
 CREATE TABLE t_factura(
     id_factura      int(11)        AUTO_INCREMENT NOT NULL,
+    id_usuario      int(11 ) AUTO_INCREMENT NOT NULL,
     fecha           date,
     id_cliente      int(11),
     id_producto     int(11),
-    cantidad        varchar(100),
+    cantidad        int(11),
     valor_unitario  float(10,2),
     CONSTRAINT      pk_factura     PRIMARY KEY(id_factura),
+    CONSTRAINT      fk_usuario     FOREIGN KEY (id_usuario), REFERENCES t_usuario (id_usuario),
     CONSTRAINT      fk_cliente     FOREIGN KEY(id_cliente) REFERENCES t_cliente(id_cliente),
     CONSTRAINT      fk_producto    FOREIGN KEY(id_producto) REFERENCES t_producto(id_producto)
-)ENGINE=InnoDb;      
+)ENGINE=InnoDb;
+
+CREATE TABLE t_rol (
+    id_rol          int(11) AUTO_INCREMENT NOT NULL  ,
+    rol             varchar (100),
+    CONSTRAINT pk_rol PRIMARY KEY (id_rol)
+)ENGINE=InnoDb;
 
 CREATE TABLE t_usuario(
-    
-)
+    id_usuario      int(11) AUTO_INCREMENT NOT NULL
+    nombre          varchar (100),
+    apellido        varchar (100),
+    correo          varchar (100),
+    password        varchar (100),
+    id_rol          int (11),
+    CONSTRAINT pk_usuario PRIMARY KEY (id_usuario),
+    CONSTRAINT fk_rol FOREIGN KEY (id_rol) REFERENCES t_rol (id_rol)
+)ENGINE=InnoDb;
 
 SELECT pro.id_producto,
 		pro.producto,
@@ -64,4 +79,23 @@ SELECT pro.id_producto,
         uni.unidad
 FROM t_producto pro
 INNER JOIN t_unidad uni ON pro.id_unidad = uni.id_unidad
+
+--Factura
+SELECT FAC.id_factura,
+       FAC.fecha,
+       FAC.id_usuario,
+       USU.nombre,
+       USU.apellido,
+       FAC.id_cliente,
+      CLIE.nombre,
+      CLIE.direccion,
+      FAC.id_producto,
+      PROD.producto,
+      FAC.cantidad,
+      FAC.valor_unitario,
+      FAC.cantidad *  FAC.valor_unitario AS Total
+FROM `t_factura`  FAC
+INNER JOIN t_usuario USU ON USU.id_usuario = FAC.id_usuario
+INNER JOIN t_cliente CLIE ON CLIE.id_cliente = FAC.id_cliente
+INNER JOIN t_producto PROD ON PROD.id_producto = FAC.id_producto
 
